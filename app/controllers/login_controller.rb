47cdcaeb0f1e4d28b67be99d9e9a5bf823aa88d9
Alpar Szotyori <sz.alpar@gmail.com>
2012-01-29 10:38:43 +0200

    Initial commit.

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,5 @@
+class LoginController < ApplicationController
+  def hello
+    
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timetable::Application.config.secret_token = '1eef67f8949925a6e698b77f38afc5e6bcb37f681585af4986e67667e81920a84ba4e2903e76b3c36846af02327b91ff95fb8dd582c55c1b123bfc04cf3ae20c'
