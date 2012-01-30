3ac951f5cb128a33e8bc692736d90f7bc156b0ec
Alpar Szotyori <sz.alpar@gmail.com>
2012-01-30 22:29:31 +0200

    Created scaffold conrollers for:  - User  - Course  - CoureType  - Hour  - Teach  - Role  - Wis  - Timesheet

---
+++ app/controllers/course_types_controller.rb
@@ -0,0 +1,83 @@
+class CourseTypesController < ApplicationController
+  # GET /course_types
+  # GET /course_types.json
+  def index
+    @course_types = CourseType.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @course_types }
+    end
+  end
+
+  # GET /course_types/1
+  # GET /course_types/1.json
+  def show
+    @course_type = CourseType.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @course_type }
+    end
+  end
+
+  # GET /course_types/new
+  # GET /course_types/new.json
+  def new
+    @course_type = CourseType.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @course_type }
+    end
+  end
+
+  # GET /course_types/1/edit
+  def edit
+    @course_type = CourseType.find(params[:id])
+  end
+
+  # POST /course_types
+  # POST /course_types.json
+  def create
+    @course_type = CourseType.new(params[:course_type])
+
+    respond_to do |format|
+      if @course_type.save
+        format.html { redirect_to @course_type, notice: 'Course type was successfully created.' }
+        format.json { render json: @course_type, status: :created, location: @course_type }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @course_type.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /course_types/1
+  # PUT /course_types/1.json
+  def update
+    @course_type = CourseType.find(params[:id])
+
+    respond_to do |format|
+      if @course_type.update_attributes(params[:course_type])
+        format.html { redirect_to @course_type, notice: 'Course type was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @course_type.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /course_types/1
+  # DELETE /course_types/1.json
+  def destroy
+    @course_type = CourseType.find(params[:id])
+    @course_type.destroy
+
+    respond_to do |format|
+      format.html { redirect_to course_types_url }
+      format.json { head :ok }
+    end
+  end
+end
---
+++ app/controllers/timesheets_controller.rb
@@ -0,0 +1,83 @@
+class TimesheetsController < ApplicationController
+  # GET /timesheets
+  # GET /timesheets.json
+  def index
+    @timesheets = Timesheet.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @timesheets }
+    end
+  end
+
+  # GET /timesheets/1
+  # GET /timesheets/1.json
+  def show
+    @timesheet = Timesheet.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @timesheet }
+    end
+  end
+
+  # GET /timesheets/new
+  # GET /timesheets/new.json
+  def new
+    @timesheet = Timesheet.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @timesheet }
+    end
+  end
+
+  # GET /timesheets/1/edit
+  def edit
+    @timesheet = Timesheet.find(params[:id])
+  end
+
+  # POST /timesheets
+  # POST /timesheets.json
+  def create
+    @timesheet = Timesheet.new(params[:timesheet])
+
+    respond_to do |format|
+      if @timesheet.save
+        format.html { redirect_to @timesheet, notice: 'Timesheet was successfully created.' }
+        format.json { render json: @timesheet, status: :created, location: @timesheet }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @timesheet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /timesheets/1
+  # PUT /timesheets/1.json
+  def update
+    @timesheet = Timesheet.find(params[:id])
+
+    respond_to do |format|
+      if @timesheet.update_attributes(params[:timesheet])
+        format.html { redirect_to @timesheet, notice: 'Timesheet was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @timesheet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /timesheets/1
+  # DELETE /timesheets/1.json
+  def destroy
+    @timesheet = Timesheet.find(params[:id])
+    @timesheet.destroy
+
+    respond_to do |format|
+      format.html { redirect_to timesheets_url }
+      format.json { head :ok }
+    end
+  end
+end
